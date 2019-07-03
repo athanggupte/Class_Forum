526619785dbc6d9cbec066cb3c337df93a5d432e
athang213 <athang213@gmail.com>
2019-07-03 23:44:11 +0530

    Added titles to both panes, Added ComboBox for Class.
    Try to customise ComboBox in next update.

--- src/core/gui/windows/LauncherWindow.java
+++ src/core/gui/windows/LauncherWindow.java
@@ -57,10 +57,12 @@ public class LauncherWindow extends JFrame{
 	groupLayout.setAutoCreateGaps(true);
 	groupLayout.setAutoCreateContainerGaps(true);
 	
+	JLabel paneTitleLabel = new JLabel("Sign In");
 	GRoundedTextField usernameField = new GRoundedTextField();
 	GRoundedPasswordField passwordField = new GRoundedPasswordField();
 	JLabel usernameLabel = new JLabel("Username");
 	JLabel passwordLabel = new JLabel("Password");
+	JLabel notRegLabel = new JLabel("Not registered yet? Sign up now!");
 	
 	GButton loginButton = new GButton("Login", colorScheme, ColorSchemes.Highlight.HL2){
 	    @Override
@@ -84,6 +86,10 @@ public class LauncherWindow extends JFrame{
 	};
 	signUpButton.setFont(new Font(signUpButton.getFont().getName(), Font.BOLD, 18));
 	
+	paneTitleLabel.setForeground(colorScheme.secondaryColor);
+	paneTitleLabel.setFont(new Font(paneTitleLabel.getFont().getName(), Font.BOLD, 24));
+	paneTitleLabel.setAlignmentX(JLabel.CENTER);
+	
 	usernameField.setBackground(colorScheme.primaryColor);
 	usernameField.setForeground(colorScheme.secondaryVariantColor);
 	usernameField.setBorderColor(colorScheme.primaryVariantColor);
@@ -96,10 +102,14 @@ public class LauncherWindow extends JFrame{
 	usernameLabel.setFont(new Font(usernameLabel.getFont().getName(), Font.BOLD, 15));
 	
 	passwordLabel.setForeground(colorScheme.secondaryColor);
-	passwordLabel.setFont(new Font(usernameLabel.getFont().getName(), Font.BOLD, 15));
+	passwordLabel.setFont(new Font(passwordLabel.getFont().getName(), Font.BOLD, 15));
+	
+	notRegLabel.setForeground(colorScheme.secondaryColor);
+	notRegLabel.setFont(new Font(notRegLabel.getFont().getName(), Font.ITALIC, 13));
 	
 	groupLayout.setHorizontalGroup(
 		groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addComponent(paneTitleLabel, GroupLayout.Alignment.CENTER)
 			.addGroup(groupLayout.createSequentialGroup()
 				.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
 					.addComponent(usernameLabel)
@@ -108,12 +118,15 @@ public class LauncherWindow extends JFrame{
 					.addComponent(usernameField)
 					.addComponent(passwordField)))
 			.addComponent(loginButton)
+			.addComponent(notRegLabel, GroupLayout.Alignment.CENTER)
 			.addComponent(signUpButton)
 		);
 	
 	groupLayout.setVerticalGroup(
 		groupLayout.createSequentialGroup()
-			.addGap(0, 150, 170)
+			.addGap(0, 60, 70)
+			.addComponent(paneTitleLabel)
+			.addGap(0, 60, 70)
 			.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				.addComponent(usernameLabel)
 				.addComponent(usernameField, 40, 40, 40))
@@ -123,9 +136,11 @@ public class LauncherWindow extends JFrame{
 				.addComponent(passwordField))
 			.addGap(15)
 			.addComponent(loginButton)
+			.addGap(0, 80, 90)
+			.addComponent(notRegLabel)
 			.addGap(15)
 			.addComponent(signUpButton, 40, 40, 40)
-			.addGap(0, 120, 170)
+			.addGap(10, 10, 40)
 	);
 	
 	groupLayout.linkSize(SwingConstants.VERTICAL, usernameField, passwordField);
@@ -151,12 +166,15 @@ public class LauncherWindow extends JFrame{
 	    }	    
 	};
 	
+	JLabel paneTitleLabel = new JLabel("Register");
 	GRoundedTextField usernameField = new GRoundedTextField();
 	GRoundedPasswordField passwordField = new GRoundedPasswordField();
 	GRoundedTextField emailField = new GRoundedTextField();
+	JComboBox classComboBox = new JComboBox(new String[]{"FE", "SE", "TE", "BE"});
 	JLabel usernameLabel = new JLabel("Username");
 	JLabel passwordLabel = new JLabel("Password");
 	JLabel emailLabel = new JLabel("E-mail");
+	JLabel classLabel = new JLabel("Class");
 	
 	GButton signUpButton = new GButton("Submit", colorScheme, ColorSchemes.Highlight.HL1){
 	    @Override
@@ -171,17 +189,27 @@ public class LauncherWindow extends JFrame{
 	};
 	signUpButton.setFont(new Font(signUpButton.getFont().getName(), Font.BOLD, 18));
 	
+	paneTitleLabel.setForeground(colorScheme.secondaryColor);
+	paneTitleLabel.setFont(new Font(paneTitleLabel.getFont().getName(), Font.BOLD, 24));
+	paneTitleLabel.setAlignmentX(JLabel.CENTER);
+	
 	usernameField.setBackground(colorScheme.primaryColor);
 	usernameField.setForeground(colorScheme.secondaryVariantColor);
 	usernameField.setBorderColor(colorScheme.primaryVariantColor);
+	usernameField.setFont(new Font(usernameField.getFont().getName(), Font.BOLD, 12));
 	
 	passwordField.setBackground(colorScheme.primaryColor);
 	passwordField.setForeground(colorScheme.secondaryVariantColor);
 	passwordField.setBorderColor(colorScheme.primaryVariantColor);
+	passwordField.setFont(new Font(passwordField.getFont().getName(), Font.BOLD, 12));
 	
 	emailField.setBackground(colorScheme.primaryColor);
 	emailField.setForeground(colorScheme.secondaryVariantColor);
 	emailField.setBorderColor(colorScheme.primaryVariantColor);
+	emailField.setFont(new Font(emailField.getFont().getName(), Font.BOLD, 12));
+	
+	classComboBox.setBackground(colorScheme.primaryColor);
+	classComboBox.setForeground(colorScheme.secondaryVariantColor);
 	
 	usernameLabel.setForeground(colorScheme.secondaryColor);
 	usernameLabel.setFont(new Font(usernameLabel.getFont().getName(), Font.BOLD, 13));
@@ -192,23 +220,34 @@ public class LauncherWindow extends JFrame{
 	emailLabel.setForeground(colorScheme.secondaryColor);
 	emailLabel.setFont(new Font(usernameLabel.getFont().getName(), Font.BOLD, 13));
 	
+	classLabel.setFont(new Font(usernameLabel.getFont().getName(), Font.BOLD, 13));
+	
 	groupLayout.setHorizontalGroup(
 		groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+			.addGroup(groupLayout.createSequentialGroup()
+				.addComponent(backButton, 32, 32, 32)
+				.addGap(0, 150, 170))
+			.addComponent(paneTitleLabel, GroupLayout.Alignment.CENTER)
 			.addGroup(groupLayout.createSequentialGroup()
 				.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
 					.addComponent(usernameLabel)
 					.addComponent(passwordLabel)
-					.addComponent(emailLabel))
+					.addComponent(emailLabel)
+					.addComponent(classLabel))
 				.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
 					.addComponent(usernameField)
 					.addComponent(passwordField)
-					.addComponent(emailField)))
+					.addComponent(emailField)
+					.addComponent(classComboBox)))
 			.addComponent(signUpButton)
 	);
 	
 	groupLayout.setVerticalGroup(
 		groupLayout.createSequentialGroup()
-			.addGap(0, 150, 170)
+			.addComponent(backButton, 23, 23, 23)
+			.addGap(10, 30, 50)
+			.addComponent(paneTitleLabel)
+			.addGap(10, 30, 50)
 			.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				.addComponent(usernameLabel)
 				.addComponent(usernameField, 30, 30, 30))
@@ -220,9 +259,12 @@ public class LauncherWindow extends JFrame{
 			.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 				.addComponent(emailLabel)
 				.addComponent(emailField))
+			.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+				.addComponent(classLabel)
+				.addComponent(classComboBox))
 			.addGap(15)
 			.addComponent(signUpButton, 40, 40, 40)
-			.addGap(0, 120, 170)
+			.addGap(10, 120, 170)
 	);
 	
 	groupLayout.linkSize(SwingConstants.VERTICAL, usernameField, passwordField, emailField);
